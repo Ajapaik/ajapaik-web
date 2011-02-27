dd4139a620d7bd009e11521339b6af5f6183b41b
Ahti Heinla <ahti@ahti.bluemoon.ee>
2011-02-27 10:52:38 +0200

    get_next_photos_to_geotag() now takes into account the difficulty level of photo

--- src/project/get_next_photos_to_geotag.py
+++ src/project/get_next_photos_to_geotag.py
@@ -12,6 +12,10 @@ def get_next_photos_to_geotag(user_id,nr_of_photos=5):
 
 	#!!! kui hea arvaja sa oled
 
+	final_level_select={'final_level':
+			"(case when level > 0 then level else " + \
+							"coalesce(guess_level,1) end)"}
+
 	forbidden_photo_ids=frozenset([g.photo_id \
 			for g in Guess.objects.filter(user=user_id,
 				created__gte= \
@@ -20,19 +24,24 @@ def get_next_photos_to_geotag(user_id,nr_of_photos=5):
 							values_list('photo_id',flat=True)))
 	known_photos=list(Photo.objects.exclude(
 								pk__in=forbidden_photo_ids). \
-					filter(confidence__gte=0.3)[:nr_of_photos])
+					filter(confidence__gte=0.3). \
+					extra(select=final_level_select). \
+					order_by('final_level')[:nr_of_photos])
 	unknown_photos=list(Photo.objects.exclude(
 								pk__in=forbidden_photo_ids). \
 					filter(confidence=0,pk__in= \
 						GeoTag.objects.values_list(
-									'photo_id',flat=True)) \
-												[:nr_of_photos])
+									'photo_id',flat=True)). \
+					extra(select=final_level_select). \
+					order_by('final_level')[:nr_of_photos])
 	if not unknown_photos:
 		unknown_photos=list(Photo.objects.exclude(pk__in= \
 						GeoTag.objects.values_list(
 									'photo_id',flat=True)). \
+				extra(select=final_level_select). \
 				annotate(skips_count=Count('guess')). \
-				order_by('skips_count')[:nr_of_photos])
+				order_by('final_level','skips_count') \
+											[:nr_of_photos])
 
 	photos=random.sample(list(set(known_photos+unknown_photos)),
 						nr_of_photos)
