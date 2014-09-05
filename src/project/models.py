0f183810599a90b2cfeaea684d32e79000a1c0c6
Lauri Elias <lauri.elias@tlu.ee>
2014-09-05 14:29:48 +0300

    Bug fix?

--- src/project/models.py
+++ src/project/models.py
@@ -385,13 +385,13 @@ class Photo(models.Model):
 					total_weight += v.trustworthiness
 				unique_correct_guesses_ratio = correct_guesses_weight / float(total_weight)
 				unique_azimuth_correct_ratio = False
-				if azimuth_correct_guesses_weight  and azimuth_total_guesses_weight > 0:
+				if azimuth_correct_guesses_weight > 0 and azimuth_total_guesses_weight > 0:
 					unique_azimuth_correct_ratio = azimuth_correct_guesses_weight / float(azimuth_total_guesses_weight)
 
 				if unique_correct_guesses_ratio > 0.63:
 					self.lon = lon_sum / float(correct_guesses_weight)
 					self.lat = lat_sum / float(correct_guesses_weight)
-					if azimuth_sum != 0 and unique_azimuth_correct_ratio:
+					if azimuth_sum != 0 and unique_azimuth_correct_ratio > 0.63:
 						self.azimuth = azimuth_sum / float(azimuth_correct_guesses_weight)
 						self.azimuth_confidence = unique_azimuth_correct_ratio * min(1, azimuth_correct_guesses_weight / 3)
 					self.confidence = unique_correct_guesses_ratio * min(1, correct_guesses_weight / 3)
