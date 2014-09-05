5cdc70708cd58d1a235356f8d9c159edde7439e9
Lauri Elias <lauri.elias@tlu.ee>
2014-09-05 16:27:57 +0300

    Fix

--- src/project/management/commands/test_photo_levelling.py
+++ src/project/management/commands/test_photo_levelling.py
@@ -10,16 +10,16 @@ class Command(BaseCommand):
 			photo_geotags = len(GeoTag.objects.filter(id=photo.id).values_list("id", flat=True))
 			photo_skips = len(Skip.objects.filter(id=photo.id).values_list("id", flat=True))
 			if photo_skips == 0:
-				output_buf.append(",".join([photo.id, photo.description, photo.confidence, "No skips", 1]))
+				output_buf.append(",".join([str(photo.id), photo.description, str(photo.confidence), "No skips", "1"]))
 			else:
 				ratio = photo_geotags / photo_skips
 				if ratio > 1.2:
-					level = 1
+					level = "1"
 				elif 1.2 >= ratio > 0.8:
-					level = 2
+					level = "2"
 				else:
-					level = 3
-				output_buf.append(",".join([photo.id, photo.description, photo.confidence, ratio, level]))
+					level = "3"
+				output_buf.append(",".join([str(photo.id), photo.description, str(photo.confidence), str(ratio), level]))
 		f = open("output.txt", "w")
 		f.write("\n".join(output_buf))
 		f.close()
