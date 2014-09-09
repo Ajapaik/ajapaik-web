3a603608036a0da9272368351e37b85ff9ce15a4
Lauri Elias <lauri.elias@tlu.ee>
2014-09-09 14:11:30 +0300

    Test esc closing notice window

--- src/project/media/js/game.js
+++ src/project/media/js/game.js
@@ -357,7 +357,7 @@
                 }
                 noticeDiv = $("#notice");
                 noticeDiv.find(".message").text(message);
-                noticeDiv.modal({escClose: false});
+                //noticeDiv.modal({escClose: false});
                 disableContinue = false;
             }, 'json');
         }
