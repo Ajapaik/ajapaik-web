c643fca097c72fe16736bbfd09ff8f3255c50080
Lauri Elias <lauri.elias@tlu.ee>
2014-09-16 14:06:51 +0300

    Serializer change

--- src/project/serializers.py
+++ src/project/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class PhotoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Photo
-		fields = ('id', 'image', 'date', 'description', 'source_key', 'source_url', 'source', 'city', 'lat', 'lon', 'azimuth', 'confidence', 'azimuth_confidence')
+		fields = ('id', 'image', 'date_text', 'description', 'source_key', 'source_url', 'source', 'city', 'lat', 'lon', 'azimuth', 'confidence', 'azimuth_confidence')
 
 class CitySerializer(serializers.ModelSerializer):
 	class Meta:
