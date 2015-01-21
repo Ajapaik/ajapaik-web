14d3124567bce4c365d18964a900650e8d280b09
Lauri <lauri.elias@tlu.ee>
2015-01-21 17:28:40 +0200

    Recent score fix?

--- project/home/models.py
+++ project/home/models.py
@@ -57,7 +57,9 @@ def calculate_recent_activity_scores():
     thousand_actions_ago = Points.objects.order_by('-created')[1000].created
     recent_actions = Points.objects.filter(created__gt=thousand_actions_ago).values('user_id').annotate(total_points=Sum('points'))
     for each in recent_actions:
-        Profile.objects.filter(user_id=each['user_id']).update(score_recent_activity=each['total_points'])
+        profile = Profile.objects.filter(user_id=each['user_id'])[:1]
+        profile.score_recent_activity = each['total_points']
+        profile.save()
 
 
 class City(models.Model):
