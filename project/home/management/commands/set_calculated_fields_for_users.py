4e4b9ef1187cf981ad136551735e3fdfd4792042
Lauri <lauri.elias@tlu.ee>
2015-01-28 12:28:28 +0200

    New command

--- project/home/management/commands/set_calculated_fields_for_users.py
+++ project/home/management/commands/set_calculated_fields_for_users.py
@@ -7,7 +7,10 @@ class Command(BaseCommand):
     args = "profile_id"
 
     def handle(self, *args, **options):
-        profile_id = args[0]
+        try:
+            profile_id = args[0]
+        except IndexError:
+            pass
         if profile_id:
             profile = Profile.objects.get(user_id=profile_id)
             profile.set_calculated_fields()
