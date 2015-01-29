54b722423d960f3a2f7fca0cc5f83d0866cefe3f
Lauriy <lauri.elias@tlu.ee>
2015-01-29 13:19:23 +0200

    Command for cleaning up orphaned points

--- project/home/management/commands/clean_up_orphaned_points_items.py
+++ project/home/management/commands/clean_up_orphaned_points_items.py
@@ -1,12 +1,22 @@
-# from django.core.management.base import BaseCommand
-# from project.home.models import GeoTag, Points
-#
-# class Command(BaseCommand):
-#     help = "Clean up points items that no longer have a rephoto or geotag"
-#     args = "photo_id"
-#
-#     def handle(self, *args, **options):
-#         rephoto_points = Points.objects.filter(action=Points.REPHOTO)
-#         for rp in rephoto_points:
-#             try:
-#                 rephoto = Photo.objects.filter(pk=rp.action_reference).get()
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand
+from project.home.models import GeoTag, Points, Photo
+
+class Command(BaseCommand):
+    help = "Clean up points items that no longer have a rephoto or geotag"
+
+    def handle(self, *args, **options):
+        rephoto_points = Points.objects.filter(action=Points.REPHOTO)
+        for rp in rephoto_points:
+            try:
+                rephoto = Photo.objects.filter(pk=rp.action_reference).get()
+            except ObjectDoesNotExist:
+                print "Deleting rephoto points for %i" % rp.action_reference
+                rp.delete()
+        geotag_points = Points.objects.filter(action=Points.GEOTAG)
+        for gp in geotag_points:
+            try:
+                geotag = GeoTag.objects.filter(pk=gp.action_reference).get()
+            except ObjectDoesNotExist:
+                print "Deleting geotag points for %i" % gp.id
+                gp.delete()
