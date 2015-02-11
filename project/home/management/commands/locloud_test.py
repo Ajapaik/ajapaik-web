be9961629caac5e06dcf458a6e4f96c05a4ab0eb
Vahur Puik <vahur@ajapaik.ee>
2015-02-11 16:40:44 +0100

    Locloud test

--- project/home/management/commands/locloud_test.py
+++ project/home/management/commands/locloud_test.py
@@ -10,7 +10,27 @@ class Command(BaseCommand):
         f = open(ABSOLUTE_PROJECT_ROOT + '/project/home/management/commands/locloud_test_data.xml', 'r')
         parser = etree.XMLParser()
         data = etree.fromstring(f.read(), parser=parser)
-        namespaces = {'edm': 'http://www.europeana.eu/schemas/edm/', 'dc': 'http://purl.org/dc/elements/1.1/'}
+        namespaces = {
+            'edm': 'http://www.europeana.eu/schemas/edm/',
+            'dc': 'http://purl.org/dc/elements/1.1/',
+            'wgs84_pos': 'http://www.w3.org/2003/01/geo/wgs84_pos',
+        }
         edmProvidedCHO = data.find('edm:ProvidedCHO', namespaces)
+        source = edmProvidedCHO.find('dc:source', namespaces)
         identifier = edmProvidedCHO.find('dc:identifier', namespaces)
-        print identifier.text
+        existing_source = None
+        try:
+            existing_source = Source.objects.get(description=source)
+        except ObjectDoesNotExist:
+            print "No such source"
+        existing_resource = None
+        if existing_source is not None:
+            try:
+                existing_resource = Photo.objects.get(source=existing_source, source_key=identifier)
+            except ObjectDoesNotExist:
+                print "No such resource"
+        if existing_resource is not None and existing_resource.lat and existing_resource.lon:
+            edmPlace = data.find('edm:Place', namespaces)
+            wgsLat = edmPlace.find('wgs84_pos:lat')
+            wgsLat.set('text', existing_resource.lat)
+        print data
