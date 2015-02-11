e88cb308a43e888cac986f87eecfc7a313003dbe
Vahur Puik <vahur@ajapaik.ee>
2015-02-11 16:24:06 +0100

    Locloud test

--- project/home/management/commands/locloud_test.py
+++ project/home/management/commands/locloud_test.py
@@ -10,4 +10,7 @@ class Command(BaseCommand):
         f = open(ABSOLUTE_PROJECT_ROOT + '/project/home/management/commands/locloud_test_data.xml', 'r')
         parser = etree.XMLParser()
         data = etree.fromstring(f.read(), parser=parser)
-        print data
+        namespaces = {'edm': 'http://www.europeana.eu/schemas/edm/', 'dc': 'http://purl.org/dc/elements/1.1/'}
+        edmProvidedCHO = data.find('edm:ProvidedCHO', namespaces)
+        identifier = edmProvidedCHO.find('dc:identifier', namespaces)
+        print identifier.text
