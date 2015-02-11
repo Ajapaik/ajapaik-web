fefe7e28d02ad6c4f542f188a70d798544461337
Vahur Puik <vahur@ajapaik.ee>
2015-02-11 15:49:53 +0100

    Locloud test

--- project/home/management/commands/locloud_test.py
+++ project/home/management/commands/locloud_test.py
@@ -1,12 +1,13 @@
 from lxml import etree
 from django.core.management.base import BaseCommand
+from project.settings import ABSOLUTE_PROJECT_ROOT
 
 class Command(BaseCommand):
     help = "Will test enriching Locloud XML"
     args = ""
 
     def handle(self, *args, **options):
-        f = open('locloud_test_data.xml', 'r')
+        f = open(ABSOLUTE_PROJECT_ROOT + '/project/home/management/commands/locloud_test_data.xml', 'r')
         parser = etree.XMLParser()
         data = etree.fromstring(f.read(), parser=parser)
         print data
