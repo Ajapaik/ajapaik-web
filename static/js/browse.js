652ff420c45ca0e154792c8444f36816fa18df0e
Lauri <lauri.elias@tlu.ee>
2015-02-17 13:15:08 +0200

    Clustering change

--- static/js/browse.js
+++ static/js/browse.js
@@ -431,7 +431,7 @@
                     markers.push(marker);
                 }
                 if (window.map.zoom > 17 || response.photos.length <= 50) {
-                    markerClustererSettings.gridSize = 1;
+                    markerClustererSettings.gridSize = 0;
                     if (window.map.zoom < 14) {
                         markerClustererSettings.gridSize = 60;
                     }
