e17e6ebb4ba6444df4168b9357b5530651085f05
Lauriy <lauri.elias@tlu.ee>
2015-03-03 14:45:35 +0200

    Command update

--- project/home/management/commands/correct_points_fields.py
+++ project/home/management/commands/correct_points_fields.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from project.home.models import Points
-
+from django.db.utils import IntegrityError
 
 class Command(BaseCommand):
     help = "Set proper foreign key data for Points objects"
@@ -8,8 +8,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         photos = Points.objects.all()
         for p in photos:
-            if p.action == Points.GEOTAG:
-                p.geotag_id = p.action_reference
-            else:
-                p.photo_id = p.action_reference
-            p.save()
+            try:
+                if p.action == Points.GEOTAG:
+                    p.geotag_id = p.action_reference
+                else:
+                    p.photo_id = p.action_reference
+                p.save()
+            except IntegrityError:
+                print "Deleting points object with reference %d" % p.action_reference
+                p.delete()
