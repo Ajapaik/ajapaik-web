5a6c6ed32667c0bfcd3420ca6d605e83cf72012a
Lauriy <lauri.elias@tlu.ee>
2015-03-09 18:25:00 +0200

    Bug fix

--- project/home/views.py
+++ project/home/views.py
@@ -245,7 +245,7 @@ def game(request):
 
     if album_selection_form.is_valid():
         ctx['album'] = Album.objects.get(pk=album_selection_form.cleaned_data['album'].id)
-        ctx['facebook_share_photos'] = ctx['album'].photos
+        ctx['facebook_share_photos'] = ctx['album'].photos.all()
         try:
             ctx['random_album_photo'] = ctx['album'].photos.filter(lat__isnull=False, lon__isnull=False).order_by('?')[0]
         except:
