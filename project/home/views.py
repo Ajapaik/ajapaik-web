c44beead2e407e8366c4d39e22ba79dab9c7a926
Lauri <lauri.elias@tlu.ee>
2015-03-10 14:46:26 +0200

    Translations update

--- project/home/views.py
+++ project/home/views.py
@@ -1031,7 +1031,8 @@ def curator_photo_upload_handler(request):
                         ret["photos"][k] = {}
                         ret["photos"][k]["success"] = True
                         ret["photos"][k]["message"] = _("Photo already exists in Ajapaik")
-        requests.post('https://graph.facebook.com/?id="' + (request.build_absolute_uri(reverse('project.home.views.game')) + '?album=' + str(album.id)) + '"&scrape=true')
+        #response = requests.post('https://graph.facebook.com', {id: (request.build_absolute_uri(reverse('project.home.views.game')))}))
+        #print response
         for cp in all_curating_points:
             total_points_for_curating += cp.points
         ret["total_points_for_curating"] = total_points_for_curating
