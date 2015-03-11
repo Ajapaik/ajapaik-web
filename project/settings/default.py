c743008fd7b658528375428ff2db6742fb2c102a
Lauri <lauri.elias@tlu.ee>
2015-03-11 12:57:52 +0200

    Trying to fix error emails

--- project/settings/default.py
+++ project/settings/default.py
@@ -100,9 +100,9 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -185,3 +185,6 @@ CACHES = {
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+DEFAULT_FROM_EMAIL = 'info@ajapaik.ee'
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
