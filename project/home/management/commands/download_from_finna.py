6228d59f20b0e0474163c35aac39372e97ff8223
Lauriy <lauri.elias@tlu.ee>
2015-03-20 17:10:47 +0200

    Finna command for cat

--- project/home/management/commands/download_from_finna.py
+++ project/home/management/commands/download_from_finna.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
         return urlunsplit((scheme, netloc, path, new_query_string, fragment))
 
     @staticmethod
-    def _create_photos_from_xml_response(xml_response, album_id):
+    def _create_photos_from_xml_response(xml_response, album):
         for elem in xml_response:
             if elem.tag == "docs":
                 new_photo = CatPhoto(
@@ -31,7 +31,6 @@ class Command(BaseCommand):
                     source=Source.objects.get(description=elem.find('institution').text),
                     source_url=elem.find("record_link").text,
                     date_text=elem.find("main_date_str").text,
-                    album=CatAlbum.objects.get(pk=album_id),
                     author=elem.find("author").text,
                 )
                 opener = urllib2.build_opener()
@@ -39,11 +38,14 @@ class Command(BaseCommand):
                 img_response = opener.open(elem.find("image_links").text)
                 new_photo.image.save("finna.jpg", ContentFile(img_response.read()))
                 new_photo.save()
+                album.photos.add(new_photo)
+                album.save()
 
     def handle(self, *args, **options):
         self.count = 1
         url = args[0]
         album_id = args[1]
+        album = CatAlbum.objects.get(pk=album_id)
         items_per_page = 20
         page = 1
         parser = etree.XMLParser()
@@ -53,7 +55,7 @@ class Command(BaseCommand):
         xml_response = data.find("response")
         number_of_items = int(xml_response.find("numFound").text)
         pages_to_get = int(ceil(number_of_items / items_per_page))
-        self._create_photos_from_xml_response(xml_response, album_id)
+        self._create_photos_from_xml_response(xml_response, album)
         if pages_to_get > 1:
             while page < pages_to_get:
                 page += 1
@@ -62,4 +64,4 @@ class Command(BaseCommand):
                 response = urllib2.urlopen(request)
                 data = etree.fromstring(response.read(), parser=parser)
                 xml_response = data.find("response")
-                self._create_photos_from_xml_response(xml_response, album_id)
+                self._create_photos_from_xml_response(xml_response, album)
