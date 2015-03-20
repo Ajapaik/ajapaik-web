c59ea51d78f460882db3b9fb5b2e12b51abf5613
Lauriy <lauri.elias@tlu.ee>
2015-03-20 17:45:09 +0200

    Pääsuke command

---
+++ project/home/management/commands/copy_over_paasuke_photos.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from PIL import Image
+from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
+from project.home.models import Album, CatPhoto
+
+
+class Command(BaseCommand):
+    help = "Make a categorizer album from an existing album"
+
+    def handle(self, *args, **options):
+        album_photos = Album.objects.get(pk=307).photos.all()
+        for each in album_photos:
+            img = Image.open('/var/garage/' + str(each.image))
+            cp = CatPhoto(
+                title=each.description,
+                description=each.description,
+                author=u'Johannes Pääsuke',
+                source=each.source,
+                source_url=each.source_url
+            )
+            cp.image.save("cat.jpg", ContentFile(img.read()))
+            cp.save()
