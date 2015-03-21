23e65f305fb72e8ab8c5438cbcb6e8c490362b2e
Lauriy <lauri.elias@tlu.ee>
2015-03-21 16:17:17 +0200

    Don't return tags for tag

--- project/home/cat.py
+++ project/home/cat.py
@@ -237,11 +237,6 @@ def cat_tag(request):
         available_cat_tags = all_cat_tags - set(CatTagPhoto.objects.filter(
             profile=tag.profile, album=tag.album, photo=tag.photo).values_list('tag__name', flat=True))
         content['state'] = str(int(round(time.time() * 1000)))
-        content['photos+'] = [
-            {
-                'id': tag.photo.id
-            }
-        ]
     else:
         content['error'] = 2
 
