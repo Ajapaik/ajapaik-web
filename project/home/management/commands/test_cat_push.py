68d5440a1e4056b4e65e96ea8cb99055813e242b
Lauri <lauri.elias@tlu.ee>
2015-04-09 16:28:20 +0300

    Test cat

--- project/home/management/commands/test_cat_push.py
+++ project/home/management/commands/test_cat_push.py
@@ -1,6 +1,7 @@
 import json
 from django.core.management.base import BaseCommand
 import requests
+from project.home.models import CatPushDevice
 from project.settings import GOOGLE_API_KEY, GCM_ENDPOINT
 
 
@@ -14,9 +15,10 @@ class Command(BaseCommand):
             'Authorization': 'key=' + GOOGLE_API_KEY
         }
 
+        push_recipient = CatPushDevice.objects.filter().first()
         values = {
-            "registration_ids": [1],
-            "data": {"msg": "Hakklihakeeks"},
+            "registration_ids": [push_recipient.token],
+            "data": {"album": 1, "title": "Hakklihakeeks"},
             "collapse_key": "message"
         }
         values = json.dumps(values)
