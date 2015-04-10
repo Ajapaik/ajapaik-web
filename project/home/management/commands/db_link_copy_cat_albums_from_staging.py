76ff43a68903ddd0591c499814e2eab30a2698d3
Lauri <lauri.elias@tlu.ee>
2015-04-10 16:25:59 +0300

    DB link command #2 and #3

---
+++ project/home/management/commands/db_link_copy_cat_albums_from_staging.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.db import connection
+
+
+class Command(BaseCommand):
+    help = "Will copy over categorizer photos from staging"
+
+    def handle(self, *args, **options):
+        cursor = connection.cursor()
+        cursor.execute('''INSERT INTO project_catalbum SELECT p.id, p.title, p.subtitle, p.image, p.created, p.modified FROM dblink('dbname=rephoto_dev user=rephoto password=reph0t0sqluser', 'SELECT id, title, subtitle, image, created, modified')
+    AS p(id integer, title VARCHAR(255), subtitle VARCHAR(255), image VARCHAR(255), created TIMESTAMP, modified TIMESTAMP);''')
