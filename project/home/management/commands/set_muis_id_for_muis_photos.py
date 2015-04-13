18a487d9773ac6716efd96a85540a2d07a984349
Lauri <lauri.elias@tlu.ee>
2015-04-13 14:28:48 +0300

    Faster?

--- project/home/management/commands/set_muis_id_for_muis_photos.py
+++ project/home/management/commands/set_muis_id_for_muis_photos.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     help = "Set muis_id for photos"
 
     def handle(self, *args, **options):
-        photos = Photo.objects.filter(source_url__contains='muis.ee', muis_id__isnull=True)
+        photos = Photo.objects.filter(source_url__contains='muis.ee', muis_id__isnull=True).all()
         for p in photos:
             p.muis_id = "oai:muis.ee:" + p.source_url.split('/')[-1]
             p.save()
