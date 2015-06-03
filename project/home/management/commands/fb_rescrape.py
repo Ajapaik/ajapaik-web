0d7b3d94a5fc4ff09c278f29a44556ef5b7a68ff
Lauri <lauri.elias@tlu.ee>
2015-06-03 12:41:42 +0300

    Comment rescraper

--- project/home/management/commands/fb_rescrape.py
+++ project/home/management/commands/fb_rescrape.py
@@ -7,10 +7,10 @@ class Command(BaseCommand):
     help = 'Rescrape all our photo URLs'
 
     def handle(self, *args, **options):
-        photos = Photo.objects.filter(pk=8361)
+        photos = Photo.objects.order_by('created')
         query_string = 'http://graph.facebook.com/?id=%s&scrape=true'
         url_template = 'http://ajapaik.ee/foto/%d/'
         for p in photos:
-            print p
+            print p.id
             url = url_template % p.id
             requests.post(query_string % url)
