343d4861b0147557aeb8f6a766acd8a3b27f219f
Lauri <lauri.elias@tlu.ee>
2015-06-03 16:59:43 +0300

    Command fix

--- project/home/management/commands/set_latest_and_earliest_comment_and_comment_count_for_all_photos.py
+++ project/home/management/commands/set_latest_and_earliest_comment_and_comment_count_for_all_photos.py
@@ -7,13 +7,17 @@ class Command(BaseCommand):
     help = "Set latest and earliest comment, comment count for all photos"
 
     def handle(self, *args, **options):
-        photos = Photo.objects.prefetch_related('comments').annotate(comment_count=Count('comments')).filter(comment_count__gt=0)
+        photos = Photo.objects.prefetch_related('comments').annotate(comment_count=Count('comments'))
         for p in photos:
             p.fb_comments_count = p.comment_count
             first_comment = p.comments.order_by('created').first()
             if first_comment:
                 p.first_comment = first_comment.created
+            else:
+                p.first_comment = None
             latest_comment = p.comments.order_by('created').last()
             if latest_comment:
                 p.latest_comment = latest_comment.created
+            else:
+                p.latest_comment = None
         Photo.bulk.bulk_update(photos, update_fields=['fb_comments_count', 'first_comment', 'latest_comment'])
