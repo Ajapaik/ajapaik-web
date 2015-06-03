e91e663801789d8d58a7bcf963988ae448235647
Lauri <lauri.elias@tlu.ee>
2015-06-03 13:54:30 +0300

    API remove required

--- project/home/forms.py
+++ project/home/forms.py
@@ -185,7 +185,7 @@ class FrontpagePagingForm(forms.Form):
 
 class ApiAlbumNearestForm(forms.Form):
     id = forms.ModelChoiceField(queryset=Album.objects.filter(is_public=True), required=False)
-    latitude = forms.FloatField(min_value=-85.05115, max_value=85)
-    longitude = forms.FloatField(min_value=-180, max_value=180)
+    latitude = forms.FloatField(min_value=-85.05115, max_value=85, required=False)
+    longitude = forms.FloatField(min_value=-180, max_value=180, required=False)
     range = forms.FloatField(required=False)
     state = forms.CharField(max_length=255, required=False)
