b2ca7e14e43c61b98d20777cd6d4e44c2671bf52
Lauri <lauri.elias@tlu.ee>
2015-06-03 12:36:20 +0300

    FB rescraper

---
+++ project/home/management/commands/fb_rescrape.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+import requests
+from project.home.models import Photo
+
+
+class Command(BaseCommand):
+    help = 'Rescrape all our photo URLs'
+
+    def handle(self, *args, **options):
+        photos = Photo.objects.all()
+        query_string = 'http://developers.facebook.com/tools/debug/og/object?q=%s'
+        url_template = 'http://ajapaik.ee/foto/%d/'
+        for p in photos:
+            print p
+            url = url_template % p.id
+            requests.post(query_string % url)
