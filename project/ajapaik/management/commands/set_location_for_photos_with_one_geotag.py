f82a8f08a138ed6025e49c0a01f43f153506fd5d
Lauri <laurileet@gmail.com>
2015-09-21 15:48:37 +0300

    Remove debug limit from command

--- project/ajapaik/management/commands/set_location_for_photos_with_one_geotag.py
+++ project/ajapaik/management/commands/set_location_for_photos_with_one_geotag.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     help = "If photo has just 1 geotag, use its coordinates"
 
     def handle(self, *args, **options):
-        photos = Photo.objects.filter(rephoto_of__isnull=True, geotag_count=1, id=9284)
+        photos = Photo.objects.filter(rephoto_of__isnull=True, geotag_count=1)
         for p in photos:
             print p.id
             p.geotags.update(is_correct=True)
