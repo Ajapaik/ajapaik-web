780b9fb77579f10ef3a3b80c9c9e5e8dbe326be6
Lauri <laurileet@gmail.com>
2015-09-22 18:10:40 +0300

    Fix difficulty_feedback differently

--- project/ajapaik/views.py
+++ project/ajapaik/views.py
@@ -1536,25 +1536,22 @@ def difficulty_feedback(request):
     if not user_profile:
         return HttpResponse("Error", status=500)
     user_trustworthiness = _calc_trustworthiness(user_profile.pk)
-    try:
-        user_last_geotag = GeoTag.objects.filter(user=user_profile).order_by("-created")[:1].get()
-        level = request.POST.get("level") or None
-        photo_id = request.POST.get("photo_id") or None
-        # FIXME: Why so many lines?
-        if user_profile and level and photo_id and user_last_geotag:
-            feedback_object = DifficultyFeedback()
-            feedback_object.user_profile = user_profile
-            feedback_object.level = level
-            feedback_object.photo_id = photo_id
-            feedback_object.trustworthiness = user_trustworthiness
-            feedback_object.geotag = user_last_geotag
-            feedback_object.save()
-            photo = Photo.objects.get(id=photo_id)
-            # FIXME: Shouldn't use costly set_calculated_fields here, maybe send extra var to lighten it
-            photo.set_calculated_fields()
-            photo.save()
-    except ObjectDoesNotExist:
-        pass
+    user_last_geotag = GeoTag.objects.filter(user=user_profile).order_by("-created").first()
+    level = request.POST.get("level") or None
+    photo_id = request.POST.get("photo_id") or None
+    # FIXME: Why so many lines?
+    if user_profile and level and photo_id and user_last_geotag:
+        feedback_object = DifficultyFeedback()
+        feedback_object.user_profile = user_profile
+        feedback_object.level = level
+        feedback_object.photo_id = photo_id
+        feedback_object.trustworthiness = user_trustworthiness
+        feedback_object.geotag = user_last_geotag
+        feedback_object.save()
+        photo = Photo.objects.get(id=photo_id)
+        # FIXME: Shouldn't use costly set_calculated_fields here, maybe send extra var to lighten it
+        photo.set_calculated_fields()
+        photo.save()
 
     return HttpResponse("OK")
 
