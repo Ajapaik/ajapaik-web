ab20442d545949ea5a82943818d9858978ab891c
Lauri <laurileet@gmail.com>
2015-09-28 15:21:58 +0300

    Create doge sized thumbnail

--- project/ajapaik/api.py
+++ project/ajapaik/api.py
@@ -190,8 +190,7 @@ def api_album_thumb(request, album_id, thumb_size=250):
     try:
         im = get_thumbnail(random_image.image, thumb_str, upscale=False)
     except AttributeError:
-        # Broken image, return Murray
-        im = urllib2.urlopen('http://rlv.zcache.com/doge_sticker-raf4b2dbd11ec4a7992e8bf94601ace75_v9wth_8byvr_512.jpg')
+        im = get_thumbnail(urllib2.urlopen('http://rlv.zcache.com/doge_sticker-raf4b2dbd11ec4a7992e8bf94601ace75_v9wth_8byvr_512.jpg'), thumb_str, upscale=False)
     content = im.read()
     response = HttpResponse(content, content_type='image/jpg')
 
