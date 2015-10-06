b72007a76bd7a2a9c20e968dbc9ba3103fb0a1b5
Lauri <laurileet@gmail.com>
2015-10-06 17:20:46 +0300

    Fix Delfi API URL

--- project/ajapaik/urls.py
+++ project/ajapaik/urls.py
@@ -95,8 +95,9 @@ urlpatterns += patterns('project.ajapaik.api',
 )
 
 urlpatterns += patterns('project.ajapaik.delfi',
+    url(r'^delfi-api/v1/photo/$', 'photo_info'),
+    url(r'^delfi_api/v1/photo/$', 'photo_info'),
     url(r'^delfi-api/v1/photos-bbox/$', 'photos_bbox'),
-    url(r'^delfi-api/v1/photo/$', 'photos_bbox'),
     url(r'^delfi_api/v1/photos_bbox/$', 'photos_bbox'),
 )
 
