a4b862a03a0941c1d56fbcb4a2a402d8adbaa37f
Lauri <laurileet@gmail.com>
2015-10-20 14:58:25 +0300

    Add rotate command to Sift, fix bug with prev/next URL slug

--- project/sift/management/commands/rotate_photo.py
+++ project/sift/management/commands/rotate_photo.py
@@ -1,18 +1,18 @@
 from PIL import Image
 from django.core.management.base import BaseCommand
-from project.ajapaik import settings
-from project.ajapaik.models import Photo
+from project.sift.models import CatPhoto
+from project.sift.settings import MEDIA_ROOT
 
 
 class Command(BaseCommand):
-    help = "Will rotate specified photo specified degrees"
+    help = "Will rotate specified Sift photo specified degrees"
     args = "photo_id, degrees"
 
     def handle(self, *args, **options):
-        photo_id = args[0]
+        photo_id = int(args[0])
         degrees = int(args[1])
-        photo = Photo.objects.filter(pk=photo_id).first()
-        photo_path = settings.MEDIA_ROOT + "/" + str(photo.image)
+        photo = CatPhoto.objects.filter(pk=photo_id).first()
+        photo_path = MEDIA_ROOT + "/" + str(photo.image)
         img = Image.open(photo_path)
         rot = img.rotate(degrees, expand=1)
         rot.save(photo_path)
