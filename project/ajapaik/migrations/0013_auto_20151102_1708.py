95244e747fc0340fe9153c8d50b7f35478d68cf8
Lauri <laurileet@gmail.com>
2015-11-02 17:09:20 +0200

    Add migrations

---
+++ project/ajapaik/migrations/0013_auto_20151102_1708.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ajapaik', '0012_auto_20151029_1745'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='geotag',
+            name='origin',
+            field=models.PositiveSmallIntegerField(default=0, choices=[(0, 'M\xe4ng'), (1, 'Kaardivaade'), (2, 'Galerii'), (3, 'P\xfcsiviide')]),
+            preserve_default=True,
+        ),
+    ]
