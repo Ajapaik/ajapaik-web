b317e4c4c04513d442aa2987ebd9fa6468bb4717
Lauri <laurileet@gmail.com>
2015-11-10 14:58:07 +0200

    Remove unnecessary libs, add tmpl.min.js, add .idea folderm, restore submitDatingURL, remove old photo_upload template, update gitignore

--- project/ajapaik/static/libs/PapaParse-4.1.0/tests/test.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var open = require('open');
-var path = require('path');
-
-connect().use(serveStatic(path.join(__dirname, '/..'))).listen(8071, function() {
-  open('http://localhost:8071/tests/tests.html');
-  console.log('Serving tests...');
-});
