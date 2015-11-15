2a9146ae5bacfd66b8786dc7b0c36c761da00e41
marju <marju.niinemaa@gmail.com>
2015-11-15 16:04:25 +0200

    Camera's photot upload

--- project/ajapaik/static/js/then-and-now-detail.js
+++ project/ajapaik/static/js/then-and-now-detail.js
@@ -4,6 +4,10 @@
         $('.glyphicon-camera').click(function () {
             $('#camera-file-capture').click();
         });
+		$('.glyphicon-ok').click(function () {
+			console.log ($('#camera-file-capture').val());
+			$('form').submit();
+		});
     });
 }());
 
@@ -14,7 +18,8 @@ var fileSelect = document.getElementById('file-select');
     function PreviewImage() {
         var oFReader = new FileReader ();
         oFReader.readAsDataURL(document.getElementById("camera-file-capture").files[0]);
-
+		$('.glyphicon-ok').show();
+		$('#uploadPreview').show();
         oFReader.onload = function (oFREvent) {
             document.getElementById("uploadPreview").src = oFREvent.target.result;
         };
