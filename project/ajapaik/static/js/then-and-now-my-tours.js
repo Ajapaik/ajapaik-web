e6b67cd93f0f5827e4f9084c513fe1b1a9c0898c
Lauri <laurileet@gmail.com>
2016-01-15 17:37:09 +0200

    Async update tour listing better, change create tour step 5 design

--- project/ajapaik/static/js/then-and-now-my-tours.js
+++ project/ajapaik/static/js/then-and-now-my-tours.js
@@ -9,7 +9,9 @@
         $('.tan-delete-tour-button').click(function () {
             if (confirm(gettext('Are you sure?'))) {
                 var $this = $(this),
-                    tourCountElement = $('#tan-my-tours-count');
+                    tourCountElement = $('#tan-my-tours-count'),
+                    participatedCountElement = $('#tan-my-participated-count'),
+                    viewedCountElement = $('#tan-my-viewed-count');
                 $.ajax({
                     method: 'POST',
                     url: deleteTourURL,
@@ -19,7 +21,11 @@
                     },
                     success: function () {
                         $this.parent().remove();
+                        $('#tan-my-tours-tour-viewed-' + $this.data('id')).remove();
+                        $('#tan-my-tours-tour-participated-' + $this.data('id')).remove();
                         tourCountElement.html(parseInt(tourCountElement.html(), 10) - 1);
+                        participatedCountElement.html(parseInt(participatedCountElement.html(), 10) - 1);
+                        viewedCountElement.html(parseInt(viewedCountElement.html(), 10) - 1);
                     },
                     error: function () {
                         alert(gettext('Cannot delete tour, has rephotos or does not belong to you.'));
