80df6f463d60463ff78d448c0afe75e0e97fe198
lauri <lauri.elias@outlook.com>
2016-02-28 11:44:04 +0200

    Try to fix out of memory kill with comment command

--- project/ajapaik/management/commands/set_latest_and_earliest_comment_and_comment_count_for_all_photos.py
+++ project/ajapaik/management/commands/set_latest_and_earliest_comment_and_comment_count_for_all_photos.py
@@ -4,20 +4,24 @@ from project.ajapaik.models import Photo
 
 
 class Command(BaseCommand):
-    help = "Set latest and earliest comment, comment count for all photos"
+    help = 'Set latest and earliest comment, comment count for all photos'
 
     def handle(self, *args, **options):
-        photos = Photo.objects.prefetch_related('comments').annotate(comment_count=Count('comments'))
-        for p in photos:
-            p.fb_comments_count = p.comment_count
-            first_comment = p.comments.order_by('created').first()
-            if first_comment:
-                p.first_comment = first_comment.created
-            else:
-                p.first_comment = None
-            latest_comment = p.comments.order_by('created').last()
-            if latest_comment:
-                p.latest_comment = latest_comment.created
-            else:
-                p.latest_comment = None
-            p.light_save()
+        photos_with_comments_count = Photo.objects.filter(comments__isnull=False).count()
+        start = 0
+        while start < photos_with_comments_count:
+            photos = Photo.objects.select_related('comments').annotate(comment_count=Count('comments'))[:start]
+            for p in photos:
+                p.fb_comments_count = p.comment_count
+                first_comment = p.comments.order_by('created').first()
+                if first_comment:
+                    p.first_comment = first_comment.created
+                else:
+                    p.first_comment = None
+                latest_comment = p.comments.order_by('created').last()
+                if latest_comment:
+                    p.latest_comment = latest_comment.created
+                else:
+                    p.latest_comment = None
+                p.light_save()
+            start += 50
