6cc9c44df18e4e5f0878ee96bcd457feebb50669
Lauri <laurileet@gmail.com>
2016-09-23 16:26:50 +0300

    Tweak photo flipping command

--- project/ajapaik/management/commands/flip_flipped_photos.py
+++ project/ajapaik/management/commands/flip_flipped_photos.py
@@ -11,6 +11,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         photos = Photo.objects.filter(flip=True, rephoto_of__isnull=True)
+        print photos.count()
         for p in photos:
             print p.id
             photo_path = settings.MEDIA_ROOT + "/" + str(p.image)
