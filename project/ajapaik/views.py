7e0f1542b6f71fdac77729f077d3bf41b8318e34
Lauri Elias <lauri@ajapaik.ee>
2016-10-23 00:32:59 +0300

    Revert test commit

--- project/ajapaik/views.py
+++ project/ajapaik/views.py
@@ -2162,7 +2162,6 @@ def curator_photo_upload_handler(request):
                                 all_curating_points.append(points_for_recurating)
                         dap = AlbumPhoto(photo=existing_photo, album=default_album, profile=profile,
                                          type=AlbumPhoto.RECURATED)
-                        # Test
                         dap.save()
                         ret["photos"][k] = {}
                         ret["photos"][k]["success"] = True
