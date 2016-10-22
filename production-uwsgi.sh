0a19d6f2a007e62adf99e004428f911ca7e22a42
Lauri Elias <laurileet@gmail.com>
2016-10-23 02:20:29 +0300

    Add more parameters to uwsgi command

--- production-uwsgi.sh
+++ production-uwsgi.sh
@@ -3,5 +3,6 @@ export HOME=/home/fotodugnad
 cd $HOME
 source venv/bin/activate
 cd $HOME/ajapaik-web/
-uwsgi --socket /home/fotodugnad/ajapaik-web/fotodugnad.sock --module project.ajapaik.wsgihandler --chmod-socket=777 --env=LANG="en_US.utf8"
+uwsgi --socket /home/fotodugnad/ajapaik-web/fotodugnad.sock --module project.ajapaik.wsgihandler --chmod-socket=777 --env=LANG="en_US.utf8" --processes 8 --master --harakiri 180 --max-requests 500
+
 exit $?
