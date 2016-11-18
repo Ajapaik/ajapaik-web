0154b0e2dbefadb2259eafdbeed7cea6e2cc1188
Lauri Elias <lauri@ajapaik.ee>
2016-11-18 14:42:44 +0200

    Try to make a more memory efficient empty user deletion command

--- project/ajapaik/management/commands/delete_zero_data_users.py
+++ project/ajapaik/management/commands/delete_zero_data_users.py
@@ -7,7 +7,9 @@ class Command(BaseCommand):
     help = 'Find and delete profiles/users that have no data attached (just people who have arrived at the page and never done anything)'
 
     def handle(self, *args, **options):
-        profiles = Profile.objects.filter(photos__isnull=True, albums__isnull=True,
+        offset = 0
+        pagesize = 1000
+        count = Profile.objects.filter(photos__isnull=True, albums__isnull=True,
                                           album_photo_links__isnull=True, datings__isnull=True,
                                           dating_confirmations__isnull=True, difficulty_feedbacks__isnull=True,
                                           geotags__isnull=True, points__isnull=True, skips__isnull=True,
@@ -19,15 +21,25 @@ class Command(BaseCommand):
                                           google_plus_name__isnull=True, google_plus_token__isnull=True,
                                           google_plus_picture__isnull=True, first_name__isnull=True,
                                           last_name__isnull=True, likes__isnull=True, tour_groups__isnull=True,
-                                          owned_tours__isnull=True, tour_rephotos__isnull=True, tour_views__isnull=True)
-        start = 0
-        end = 10000
-        while end < 8000000:
-            profiles_slice = profiles[start:end]
-            for each in profiles_slice:
+                                          owned_tours__isnull=True, tour_rephotos__isnull=True, tour_views__isnull=True).count()
+        print count
+        while offset < count:
+            for m in Profile.objects.filter(photos__isnull=True, albums__isnull=True,
+                                          album_photo_links__isnull=True, datings__isnull=True,
+                                          dating_confirmations__isnull=True, difficulty_feedbacks__isnull=True,
+                                          geotags__isnull=True, points__isnull=True, skips__isnull=True,
+                                          fb_name__isnull=True, fb_link__isnull=True, fb_id__isnull=True,
+                                          fb_token__isnull=True, fb_hometown__isnull=True, fb_birthday__isnull=True,
+                                          fb_current_location__isnull=True, fb_email__isnull=True,
+                                          fb_user_friends__isnull=True, google_plus_id__isnull=True,
+                                          google_plus_email__isnull=True, google_plus_link__isnull=True,
+                                          google_plus_name__isnull=True, google_plus_token__isnull=True,
+                                          google_plus_picture__isnull=True, first_name__isnull=True,
+                                          last_name__isnull=True, likes__isnull=True, tour_groups__isnull=True,
+                                          owned_tours__isnull=True, tour_rephotos__isnull=True, tour_views__isnull=True)[offset: offset + pagesize].iterator:
                 try:
-                    each.user.delete()
+                    m.user.delete()
+                    print 'deleted %s' % m.user.id
                 except:
-                    print 'failed to delete %s' % each.get_display_name()
-            start += 10000
-            end += 10000
+                    print 'failed to delete %s' % m.user.id
+            offset += pagesize
