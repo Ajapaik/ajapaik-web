961d10a7af8b2c94d52d3ea855f08ae6ccd27327
Lauri Elias <lauri@ajapaik.ee>
2016-12-24 12:22:32 +0200

    Try to fix reverse geocoding command running out of memory

--- project/ajapaik/management/commands/reverse_geocode_photos.py
+++ project/ajapaik/management/commands/reverse_geocode_photos.py
@@ -7,7 +7,13 @@ class Command(BaseCommand):
     help = 'Will try to reverse geocode all photos'
 
     def handle(self, *args, **options):
+        start = 0
+        end = 10000
         photos = Photo.objects.filter(rephoto_of__isnull=True)
-        for p in photos:
-            p.reverse_geocode_location()
-            p.light_save()
+        count = photos.count()
+        while end < count:
+            for p in photos[start:end]:
+                p.reverse_geocode_location()
+                p.light_save()
+            start += 10000
+            end += 10000
