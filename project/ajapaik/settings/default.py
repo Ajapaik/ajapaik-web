9c631de92342020311f321fdadf7d6f6d4b355d3
Lauri Elias <laurileet@gmail.com>
2017-01-04 12:13:07 +0200

    Send error emails to admins ignoring DEBUG setting

--- project/ajapaik/settings/default.py
+++ project/ajapaik/settings/default.py
@@ -232,15 +232,9 @@ GOOGLE_PLUS_OAUTH2_CALLBACK_URL = None
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
