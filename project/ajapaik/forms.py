323e01c9144a035b67848d7eb4141eb22ab73ab4
kristokuiv <kristokuiv@gmail.com>
2017-07-03 19:26:07 +0300

    Increase password max length for api requests because of Google ID_token

--- project/ajapaik/forms.py
+++ project/ajapaik/forms.py
@@ -13,7 +13,7 @@ class APILoginAuthForm(forms.Form):
     type = forms.CharField(max_length=255)
     username = forms.CharField(max_length=255)
     # For Google+ oAuth
-    password = forms.CharField(max_length=1024)
+    password = forms.CharField(max_length=1105)
     version = forms.FloatField(required=False)
     length = forms.IntegerField(required=False, initial=0)
     os = forms.CharField(max_length=255, required=False, initial='android')
