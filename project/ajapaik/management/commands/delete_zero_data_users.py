96abdca1006be367215bdab553500bfa07f08f9c
Vadim Zabolotniy <zabolotniy.v.y@gmail.com>
2018-02-06 09:43:52 +0200

    Just code formatting in 'delete_zero_data_users' command.

--- project/ajapaik/management/commands/delete_zero_data_users.py
+++ project/ajapaik/management/commands/delete_zero_data_users.py
@@ -9,20 +9,47 @@ class Command(BaseCommand):
     help = 'Find and delete profiles/users that have no data attached (just people who have arrived at the page and never done anything)'
 
     def handle(self, *args, **options):
-        profiles = Profile.objects.filter(photos__isnull=True, albums__isnull=True,
-                                          album_photo_links__isnull=True, datings__isnull=True,
-                                          dating_confirmations__isnull=True, difficulty_feedbacks__isnull=True,
-                                          geotags__isnull=True, points__isnull=True, skips__isnull=True,
-                                          fb_name__isnull=True, fb_link__isnull=True, fb_id__isnull=True,
-                                          fb_token__isnull=True, fb_hometown__isnull=True, fb_birthday__isnull=True,
-                                          fb_current_location__isnull=True, fb_email__isnull=True,
-                                          fb_user_friends__isnull=True, google_plus_id__isnull=True,
-                                          google_plus_email__isnull=True, google_plus_link__isnull=True,
-                                          google_plus_name__isnull=True, google_plus_token__isnull=True,
-                                          google_plus_picture__isnull=True, first_name__isnull=True,
-                                          last_name__isnull=True, likes__isnull=True, tour_groups__isnull=True,
-                                          owned_tours__isnull=True, tour_rephotos__isnull=True, tour_views__isnull=True,
-                                          deletion_attempted__isnull=True)
+        profiles = Profile.objects.filter(
+            # Own fields
+            first_name__isnull=True,
+            last_name__isnull=True,
+
+            fb_name__isnull=True,
+            fb_link__isnull=True,
+            fb_id__isnull=True,
+            fb_token__isnull=True,
+            fb_hometown__isnull=True,
+            fb_current_location__isnull=True,
+            fb_birthday__isnull=True,
+            fb_email__isnull=True,
+            fb_user_friends__isnull=True,
+
+            google_plus_id__isnull=True,
+            google_plus_email__isnull=True,
+            google_plus_link__isnull=True,
+            google_plus_name__isnull=True,
+            google_plus_token__isnull=True,
+            google_plus_picture__isnull=True,
+
+            deletion_attempted__isnull=True,
+
+            # Related model fields
+            photos__isnull=True,
+            albums__isnull=True,
+            album_photo_links__isnull=True,
+            datings__isnull=True,
+            dating_confirmations__isnull=True,
+            difficulty_feedbacks__isnull=True,
+            geotags__isnull=True,
+            points__isnull=True,
+            skips__isnull=True,
+            likes__isnull=True,
+            tour_groups__isnull=True,
+            owned_tours__isnull=True,
+            tour_rephotos__isnull=True,
+            tour_views__isnull=True,
+        )
+
         start = 0
         end = 10000
         while end < 2000000:
