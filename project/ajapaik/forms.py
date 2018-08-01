9fec937a9839839ae6ddd298bf4248c2e8042e77
zache-fi <kimmo.virtanen@wikimedia.fi>
2018-08-01 19:52:32 +0300

    firstname, lastname questions to signup

--- project/ajapaik/forms.py
+++ project/ajapaik/forms.py
@@ -526,3 +526,12 @@ class ApiUserRephotoSearchForm(forms.Form):
 
 class ApiAlbumSearchForm(forms.Form):
     query = forms.CharField()
+
+class SignupForm(forms.Form):
+    first_name = forms.CharField(max_length=30, label='First name')
+    last_name = forms.CharField(max_length=30, label='Last name')
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
