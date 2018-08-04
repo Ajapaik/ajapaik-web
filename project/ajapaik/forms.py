38c2683c5409d373260b780c9dd70c59d1cd5450
zache-fi <kimmo.virtanen@wikimedia.fi>
2018-08-04 03:46:09 +0300

    Added "First name", "Last name" translations

--- project/ajapaik/forms.py
+++ project/ajapaik/forms.py
@@ -528,8 +528,8 @@ class ApiAlbumSearchForm(forms.Form):
     query = forms.CharField()
 
 class SignupForm(forms.Form):
-    first_name = forms.CharField(max_length=30, label='First name')
-    last_name = forms.CharField(max_length=30, label='Last name')
+    first_name = forms.CharField(max_length=30, label=_('First name'))
+    last_name = forms.CharField(max_length=30, label=_('Last name'))
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
