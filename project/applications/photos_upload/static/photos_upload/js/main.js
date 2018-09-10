1329fc383781758d2157b26488be24d6dda82ed5
Vadim Zabolotniy <zabolotniy.v.y@gmail.com>
2018-09-10 09:14:20 +0300

    Added massive photo name and description editing.

--- project/applications/photos_upload/static/photos_upload/js/main.js
+++ project/applications/photos_upload/static/photos_upload/js/main.js
@@ -34,6 +34,27 @@ const store = new Vuex.Store({
         setUserProfile: (state, profile) => {state.user = profile;},
 
         setLicences: (state, licences) => {state.licences = licences;},
+
+        massive_name_editing: (state, {name}) => {
+            for(var i = 0; i < state.photos.length; i++) {
+                photo = state.photos[i];
+                photo.name = `${name} ${i + 1}`;
+            }
+        },
+
+        reset_names: (state) => {
+            for(var i = 0; i < state.photos.length; i++) {
+                photo = state.photos[i];
+                photo.name = photo.originalFileName;
+            }
+        },
+
+        massive_description_editing: (state, description) => {
+            for(var i = 0; i < state.photos.length; i++) {
+                photo = state.photos[i];
+                photo.description = description;
+            }
+        },
     },
     actions: {
         fetchUserProfile: async (context) => {
@@ -114,8 +135,9 @@ Vue.component('photos-editor', {
 
             name: '',
             description: '',
-            longitude: 0,
-            latitude: 0,
+            // Geographical centre of Estonia
+            latitude: 58.657777777777774,  // 58°39'28"N
+            longitude: 25.573888888888888,  // 25°34'26"E
             azimuth: 0,
             isUserAuthor: false,
             selectedLicence: null,
@@ -131,7 +153,19 @@ Vue.component('photos-editor', {
         axios.post('/api/v1/albums/', data)
             .then(response => {this.availableAlbums = response.data.albums})
     },
-    methods: {},
+    methods: {
+        update_names: (name) => {
+            if (name) {
+                store.commit('massive_name_editing', {name: name});
+            }
+            else {
+                store.commit('reset_names');
+            }
+        },
+        update_descriptions: (description) => {
+            store.commit('massive_description_editing', description);
+        }
+    },
 });
 
 
