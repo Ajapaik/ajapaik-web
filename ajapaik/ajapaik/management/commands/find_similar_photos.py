447686c5631edbf915a3508d8f5e83379e335fce
Märt Häkkinen <mart.hakkinen@itcollege.ee>
2019-04-25 01:24:51 +0300

    feat(commands): remove unused import

--- ajapaik/ajapaik/management/commands/find_similar_photos.py
+++ ajapaik/ajapaik/management/commands/find_similar_photos.py
@@ -4,8 +4,7 @@ import requests
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from ajapaik.phash import phash
-from ajapaik.models import Photo
+from ajapaik.ajapaik.models import Photo
 
 class Command(BaseCommand):
     help = 'Calculate perceptual hash for images and then find similar images from all added images'
