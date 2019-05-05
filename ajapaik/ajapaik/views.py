8cb91d5ec24c15f5b95b9622d1c7271bae12ec52
Märt Häkkinen <mart.hakkinen@itcollege.ee>
2019-05-05 23:07:51 +0300

    feat(*): hide similar photos filter and do not add phash to new images

--- ajapaik/ajapaik/views.py
+++ ajapaik/ajapaik/views.py
@@ -490,7 +490,7 @@ def rephoto_upload(request, photo_id):
 					each.light_save()
 				re_photo.image.save('rephoto.jpg', file_obj)
 				# Image saved to disk, can analyse now
-				re_photo.find_similar()
+				# re_photo.find_similar()
 				new_id = re_photo.pk
 				img = Image.open(settings.MEDIA_ROOT + '/' + str(re_photo.image))
 				_extract_and_save_data_from_exif(re_photo)
@@ -2220,7 +2220,7 @@ def curator_photo_upload_handler(request):
 								new_photo.set_calculated_fields()
 							new_photo.image
 							new_photo.save()
-							new_photo.find_similar()
+							#new_photo.find_similar()
 							points_for_curating = Points(action=Points.PHOTO_CURATION, photo=new_photo, points=50,
 														 user=profile, created=new_photo.created,
 														 album=general_albums[0])
@@ -2743,7 +2743,7 @@ def user_upload(request):
 				photo.author = request.user.profile.get_display_name()
 				photo.licence = Licence.objects.get(id=17)  # CC BY 4.0
 			photo.save()
-			photo.find_similar()
+			#photo.find_similar()
 			for each in form.cleaned_data['albums']:
 				AlbumPhoto(
 					photo=photo,
