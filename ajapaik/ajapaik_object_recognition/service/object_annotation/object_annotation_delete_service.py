dd7ea20254e65bc5f88a037738beaef5b120905f
hansmu <hans.miikael@gmail.com>
2019-08-14 19:07:57 +0300

    Add file to handle object annotation deletion, separate it from the view as the view should handle accepting requests and delegation

---
+++ ajapaik/ajapaik_object_recognition/service/object_annotation/object_annotation_delete_service.py
@@ -0,0 +1,24 @@
+from ajapaik.ajapaik_object_recognition import object_annotation_utils
+from ajapaik.ajapaik_object_recognition.domain.annotation_remove_request import AnnotationRemove
+from ajapaik.ajapaik_object_recognition.models import ObjectDetectionAnnotation
+
+
+def remove_annotation(annotation_remove_request: AnnotationRemove) -> bool:
+    user_id = annotation_remove_request.user_id
+
+    object_detection_annotation = ObjectDetectionAnnotation.objects.get(
+        pk=annotation_remove_request.annotation_id,
+        user_id=user_id
+    )
+
+    is_deletable = object_annotation_utils.is_annotation_deletable(
+        user_id,
+        object_detection_annotation.created_on,
+        object_detection_annotation.user_id
+    )
+
+    if is_deletable:
+        object_detection_annotation.delete()
+        return True
+
+    return False
