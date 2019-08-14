1cd4ef90de5d20f456450655799ed4912e8a57ed
hansmu <hans.miikael@gmail.com>
2019-08-14 17:04:04 +0300

    Add popover for adding new annotations

---
+++ ajapaik/ajapaik/static/js/annotations/popovers/annotation.popover.new.js
@@ -0,0 +1,90 @@
+'use strict';
+
+function getRectangleSubmitFunction(scaledRectangle, popoverId) {
+    return function(event) {
+        event.preventDefault();
+
+        var form = $(event.target);
+
+        var isObjectSelected = form.data('selected') === 'object';
+
+        var selectedObjectId = form.find("#" + constants.elements.OBJECT_CLASS_SELECT_ID).val();
+        var personId = form.find('#' + constants.elements.SUBJECT_AUTOCOMPLETE_ID).val();
+
+        togglePopover(popoverId);
+
+        var payload = {
+            objectId: isObjectSelected ? selectedObjectId : null,
+            subjectId: !isObjectSelected ? personId : null,
+            photoId: scaledRectangle.photoId,
+            x1: scaledRectangle.x1,
+            x2: scaledRectangle.x2,
+            y1: scaledRectangle.y1,
+            y2: scaledRectangle.y2
+        };
+
+        addNewDetectionAnnotation(payload, refreshAnnotations);
+    };
+}
+
+function toggleFaceDetection() {
+    $('#' + constants.elements.SELECT_OBJECT_CLASS_WRAPPER_ID).hide();
+    $('#' + constants.elements.AUTOCOMPLETE_WRAPPER_ID).show();
+    $('#' + constants.elements.NEW_ANNOTATION_FORM_ID).data('selected', 'face');
+}
+
+function toggleObjectDetection() {
+    $('#' + constants.elements.SELECT_OBJECT_CLASS_WRAPPER_ID).show();
+    $('#' + constants.elements.AUTOCOMPLETE_WRAPPER_ID).hide();
+    $('#' + constants.elements.NEW_ANNOTATION_FORM_ID).data('selected', 'object');
+}
+
+function createObjectAssigningPopoverContent(popoverId, scaledRectangle) {
+    var faceLabel = gettext(constants.translations.popover.labels.FACE_ANNOTATION);
+    var objectLabel = gettext(constants.translations.popover.labels.OBJECT_ANNOTATION);
+
+    var select = getObjectsSelect();
+    var controlButtons = getSubmitAndCancelButtons(popoverId, true);
+    var detectionTypeToggle = getToggleButton(objectLabel, faceLabel, toggleObjectDetection, toggleFaceDetection);
+    var autocomplete = getPersonAutoComplete();
+
+    var formWrapper = $('<div></div>');
+
+    var form = $('<form>', {
+        id: constants.elements.NEW_ANNOTATION_FORM_ID,
+        submit: getRectangleSubmitFunction(scaledRectangle, popoverId),
+        'data-selected': 'object'
+    });
+
+    var wrapper = $('<div style="padding-top: 5px;"></div>');
+    var selectWrapper = $('<div style="padding-bottom: 15px;"></div>');
+
+    return formWrapper.append(
+        form
+            .append(wrapper
+                .append(detectionTypeToggle)
+                .append(autocomplete)
+            )
+            .append(selectWrapper
+                .append(select)
+            )
+            .append(controlButtons)
+    );
+}
+
+function createNewDetectionRectangle(popoverId, scaledRectangle, configuration) {
+    var onAnnotationRectangleClick = function() {
+        setTimeout(function() {
+            initializeAutocomplete(constants.elements.SUBJECT_AUTOCOMPLETE_ID);
+
+            new SlimSelect({
+              select: '#' + constants.elements.OBJECT_CLASS_SELECT_ID
+            });
+        }, 200);
+    };
+
+    var popoverTitle = gettext(constants.translations.popover.titles.NEW_ANNOTATION) + '?';
+    var popoverContent = createObjectAssigningPopoverContent(popoverId, scaledRectangle);
+
+    return createAnnotationRectangleWithPopover(popoverId, popoverTitle, popoverContent, configuration, onAnnotationRectangleClick);
+}
