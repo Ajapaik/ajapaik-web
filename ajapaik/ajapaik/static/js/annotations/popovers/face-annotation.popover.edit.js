9e41a233a406dfc4aededdcdb4504f4fce3a27e2
hansmu <hans.miikael@gmail.com>
2019-08-14 17:04:43 +0300

    Add popover for editing user's submitted face annotation

---
+++ ajapaik/ajapaik/static/js/annotations/popovers/face-annotation.popover.edit.js
@@ -0,0 +1,81 @@
+'use strict';
+
+function getRemoveAnnotationFunction(annotationId, popoverId) {
+    var onSuccess = function() {
+        togglePopover(popoverId);
+        refreshAnnotations();
+    };
+
+    return function() {
+        removeFaceAnnotation(annotationId, onSuccess);
+    };
+}
+
+function getModifySubmitFunction(annotationId, popoverId) {
+    var onSuccess = function() {
+        togglePopover(popoverId);
+        refreshAnnotations();
+    };
+
+    return function(event) {
+        event.preventDefault();
+
+        var form = $(event.target);
+
+        var newSubjectId = form
+            .find('#' + constants.elements.SUBJECT_AUTOCOMPLETE_ID)
+            .val();
+
+        var payload = {
+            annotationId: annotationId,
+            newSubjectId: newSubjectId
+        };
+
+        editFaceAnnotation(annotationId, payload, onSuccess);
+    };
+}
+
+function getDefaultValue(detectionRectangle) {
+    if (detectionRectangle.subjectId) {
+        return {
+            label: detectionRectangle.subjectName,
+            id: detectionRectangle.subjectId
+        };
+    }
+}
+
+function createDetectedFaceModifyPopoverContent(annotation, popoverId) {
+    var defaultValue = getDefaultValue(annotation);
+    var changeExistingFaceLabel = gettext(constants.translations.popover.labels.CHANGE_PERSON_NAME) + ':';
+
+    var autocomplete = getPersonAutoComplete(true, 'width: 180px;', defaultValue, changeExistingFaceLabel);
+    var buttons = [
+        getSubmitButton('margin-top: 10px;'),
+        getDeleteButton(getRemoveAnnotationFunction(annotation.id, popoverId)),
+        getCancelButton(popoverId)
+    ];
+
+    var buttonGroup = getButtonGroup(buttons);
+
+    var form = $('<form/>', {
+        id: 'modify-detected-object-annotation'
+    }).on('submit', getModifySubmitFunction(annotation.id, popoverId));
+
+    return form
+        .append(autocomplete)
+        .append(buttonGroup);
+}
+
+function createFaceAnnotationEditRectangle(popoverId, annotation, configuration) {
+    var onAnnotationRectangleClick = function() {
+      setTimeout(function() {
+            initializeAutocomplete(constants.elements.SUBJECT_AUTOCOMPLETE_ID);
+        }, 200);
+    };
+
+    var popoverTitle = gettext(constants.translations.popover.titles.EDIT_FACE_ANNOTATION);
+
+    var popoverContent = createDetectedFaceModifyPopoverContent(annotation, popoverId);
+
+    return createAnnotationRectangleWithPopover(popoverId, popoverTitle, popoverContent, configuration, onAnnotationRectangleClick);
+}
