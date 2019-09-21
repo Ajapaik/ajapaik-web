ce716972d75f71830b3bcfd914d1476aae5e7a58
hansmu <hans.miikael@gmail.com>
2019-09-21 16:32:45 +0300

    Add fallback language for translations, as not all concepts are translated into languages e.g. 'Skript'

--- ajapaik/ajapaik/curator_drivers/wikidata.py
+++ ajapaik/ajapaik/curator_drivers/wikidata.py
@@ -8,10 +8,11 @@ WIKI_DATA_ACTION_TRANSLATE = 'wbgetentities'
 WIKI_DATA_FORMAT = 'json'
 
 TRANSLATIONS_IN_USE = 'et|ru|en'
+FALLBACK_TRANSLATION = 'en'
 
 
 def get_label_translation(label_id):
-    global WIKI_DATA_URL, WIKI_DATA_FORMAT, WIKI_DATA_ACTION_TRANSLATE
+    global WIKI_DATA_URL, WIKI_DATA_FORMAT, WIKI_DATA_ACTION_TRANSLATE, FALLBACK_TRANSLATION
 
     params = {
         'action': WIKI_DATA_ACTION_TRANSLATE,
@@ -27,7 +28,9 @@ def get_label_translation(label_id):
     translations = result.json()['entities'][label_id]['labels']
 
     def get_translation(language):
-        return translations[language]['value']
+        has_language_translation = language in translations
+        fallback_translation = translations[FALLBACK_TRANSLATION]['value']
+        return translations[language]['value'] if has_language_translation else fallback_translation
 
     return json.dumps({
         'en': get_translation('en'),
