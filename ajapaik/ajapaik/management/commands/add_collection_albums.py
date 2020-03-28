fbf8d65e248bb7eec257867f5c8b5ebccc1bde2b
Märt Häkkinen <m4rth4kkinen@gmail.com>
2020-03-28 09:09:16 +0200

    fix(commands): exclude typo

--- ajapaik/ajapaik/management/commands/add_collection_albums.py
+++ ajapaik/ajapaik/management/commands/add_collection_albums.py
@@ -5,18 +5,23 @@ class Command(BaseCommand):
     help = 'Add collection albums and assign each photo that has source to some album'
 
     def handle(self, *args, **options):
-        photos =  Photo.objects.exclude(source__isnull)
+        photos =  Photo.objects.exclude(source_id__isnull=True)
         for photo in photos:
-            sourceAlbum = Album.objects.filter(source_id=self.source_id).first()
+            sourceAlbum = Album.objects.filter(source_id=photo.source_id).first()
             if sourceAlbum is None:
                 sourceAlbum = Album(
-                    name = self.source.name,
-                    slug = self.source.name,
+                    name = photo.source.name,
+                    slug = photo.source.name,
                     atype = Album.COLLECTION,
-                    cover_photo = self,
-                    source = self.source
+                    cover_photo = photo,
+                    source = photo.source
                 )
                 sourceAlbum.save()
+            AlbumPhoto(
+                type=AlbumPhoto.COLLECTION,
+                photo=photo,
+                album=sourceAlbum
+            ).save()
         sources = Source.objects().all()
         for source in sources:
             album = Album.objects.filter(source_id=source.id).first()
