1595183e4b3b374f08c5c5f316380a8d3a5f82cd
Märt Häkkinen <m4rth4kkinen@gmail.com>
2020-05-01 22:00:10 +0300

    feat(commands): update reset gender command

--- ajapaik/ajapaik_face_recognition/management/commands/reset_genders.py
+++ ajapaik/ajapaik_face_recognition/management/commands/reset_genders.py
@@ -1,4 +1,5 @@
 # encoding: utf-8 
+from django.db.models import Q
 from django.core.management.base import BaseCommand 
 from ajapaik.ajapaik.models import Album, Photo 
 from ajapaik.ajapaik_face_recognition.models import FaceRecognitionRectangle 
@@ -6,13 +7,9 @@ from ajapaik.ajapaik_face_recognition.models import FaceRecognitionRectangle
 class Command(BaseCommand):
     help = "Resets gender of face annotations, that are shown as 'unsure'" 
     def handle(self, *args, **options):
-        rectangles = FaceRecognitionRectangle.objects.exclude(subject_consensus__isnull=True).filter(gender__gte=2)
+        rectangles = FaceRecognitionRectangle.objects.exclude(subject_consensus__isnull=True)
         for rectangle in rectangles: 
             album = Album.objects.filter(id=rectangle.subject_consensus.id).first()
-            if(album.gender is not None and rectangle.gender is not None):
-                if(album.gender==0):
-                    rectangle.gender = 1
-                    rectangle.save() 
-                if(album.gender==1):
-                    rectangle.gender = 0 
-                    rectangle.save() 
+            if(album.gender is not None):
+                rectangle.gender = album.gender
+                rectangle.save() 
