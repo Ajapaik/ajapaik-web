ec9658930c87a381139260e81d0888e682a2fce7
Märt Häkkinen <m4rth4kkinen@gmail.com>
2020-08-03 14:00:10 +0300

    fix(settings): set default pagination class for rest framework

--- ajapaik/settings/default.py
+++ ajapaik/settings/default.py
@@ -225,6 +225,7 @@ LOGIN_URL = '/admin/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
