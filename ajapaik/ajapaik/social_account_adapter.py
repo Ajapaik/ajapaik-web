f2b49535d1b8bafc6afdf03c0db644d9c3f8d428
Märt Häkkinen <m4rth4kkinen@gmail.com>
2020-08-28 15:56:09 +0300

    feat(ajapaik): add save user to 'MySocialAccountAdapter' to save user display name from username

--- ajapaik/ajapaik/social_account_adapter.py
+++ ajapaik/ajapaik/social_account_adapter.py
@@ -1,8 +1,16 @@
 from django.urls import reverse
 from django.conf import settings
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.socialaccount.models import SocialAccount
 
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def get_connect_redirect_url(self, request, socialaccount):
         return reverse('user_settings')
+
+    def save_user(self, request, user, form, commit=True):
+        user = super(MySocialAccountAdapter, self).save_user(request, user, form)
+        social_account = SocialAccount.objects.filter(user=user).first()
+        if social_account and social_account.extra_data.get('username'):
+            user.profile.display_name = social_account.extra_data.get('username')
+            user.profile.save()
