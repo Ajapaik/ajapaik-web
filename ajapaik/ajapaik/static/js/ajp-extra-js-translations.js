e58bf0123a049f9a22dfab95331a4c697609d51a
Märt Häkkinen <m4rth4kkinen@gmail.com>
2020-08-30 02:00:23 +0300

    feat(js): add dummy js translations

--- ajapaik/ajapaik/static/js/ajp-extra-js-translations.js
+++ ajapaik/ajapaik/static/js/ajp-extra-js-translations.js
@@ -44,4 +44,6 @@
     gettext('Title');
     gettext('URL');
     gettext('Unknown author');
+    gettext('You have opened the map with the album filter on, to turn it off, please untick the box next to album name');
+    gettext('You have opened the map with the album filter off, to turn it on, please tick the box next to album name');
 }());
