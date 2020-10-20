deccfa93cc5292cd0320f4f066eb7564b9562f2f
Märt Häkkinen <m4rth4kkinen@gmail.com>
2020-10-20 03:09:19 +0300

    feat(commands): add all aerial photography album ids to viewpoint elevation import command

--- ajapaik/ajapaik/management/commands/add_viewpoint_elevation_suggestions_from_aerophotography_album.py
+++ ajapaik/ajapaik/management/commands/add_viewpoint_elevation_suggestions_from_aerophotography_album.py
@@ -6,11 +6,12 @@ class Command(BaseCommand):
     help = "Add viewpoint elevation suggestions aerophotography albums"
 
     def handle(self, *args, **options):
-        album_photos = AlbumPhoto.objects.filter(album_id=18893)
-        for album_photo in album_photos:
-            if album_photo.profile:
-                new_suggestion = PhotoViewpointElevationSuggestion(proposer=album_photo.profile, photo=album_photo.photo, viewpoint_elevation=2, created=album_photo.created)
-                new_suggestion.save()
-                album_photo.photo.viewpoint_elevation = 2
-                album_photo.photo.save()
-                Points(user=album_photo.profile, action=Points.ADD_VIEWPOINT_ELEVATION, photo=album_photo.photo, points=20,created=album_photo.created).save()
+        for album_id in [199, 5809, 18893]:
+            album_photos = AlbumPhoto.objects.filter(album_id=album_id)
+            for album_photo in album_photos:
+                if album_photo.profile:
+                    new_suggestion = PhotoViewpointElevationSuggestion(proposer=album_photo.profile, photo=album_photo.photo, viewpoint_elevation=2, created=album_photo.created)
+                    new_suggestion.save()
+                    album_photo.photo.viewpoint_elevation = 2
+                    album_photo.photo.save()
+                    Points(user=album_photo.profile, action=Points.ADD_VIEWPOINT_ELEVATION, photo=album_photo.photo, points=20,created=album_photo.created).save()
