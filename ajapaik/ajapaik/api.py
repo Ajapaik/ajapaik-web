8083545b60a28ea3f61e53c292c1e982b8cbce0d
Märt Häkkinen <m4rth4kkinen@gmail.com>
2020-10-24 01:05:37 +0300

    feat(ajapaik): show category consensus with a dark outline

--- ajapaik/ajapaik/api.py
+++ ajapaik/ajapaik/api.py
@@ -1708,7 +1708,9 @@ class PhotoSuggestion(AjapaikAPIView):
         try:
             photo = get_object_or_404(Photo, id=photo_id)
             scene = 'undefined'
+            scene_consensus = 'undefined'
             viewpoint_elevation = 'undefined'
+            viewpoint_elevation_consensus = 'undefined'
             viewpoint_elevation_suggestion = PhotoViewpointElevationSuggestion.objects.filter(photo=photo, proposer=request.user.profile).order_by('-created').first()
             if viewpoint_elevation_suggestion:
                 if viewpoint_elevation_suggestion.viewpoint_elevation == 0:
@@ -1718,12 +1720,28 @@ class PhotoSuggestion(AjapaikAPIView):
                 if viewpoint_elevation_suggestion.viewpoint_elevation == 2:
                     viewpoint_elevation = 'Aerial'
 
+
             scene_suggestion = PhotoSceneSuggestion.objects.filter(photo=photo, proposer=request.user.profile).order_by('-created').first()
-            if scene_suggestion and scene_suggestion.scene == 0:
-                scene = 'Interior'
-            if scene_suggestion and scene_suggestion.scene == 1:
-                scene = 'Exterior'
-            return JsonResponse({'scene': scene, 'viewpoint_elevation': viewpoint_elevation})
+
+            if scene_suggestion:
+                if scene_suggestion.scene == 0:
+                    scene = 'Interior'
+                if scene_suggestion.scene == 1:
+                    scene = 'Exterior'
+            
+            if photo.scene == 0:
+                scene_consensus = 'Interior'
+            if photo.scene == 1:
+                scene_consensus = 'Exterior'
+
+            if photo.viewpoint_elevation == 0:
+                viewpoint_elevation_consensus = 'Ground'
+            if photo.viewpoint_elevation == 1:
+                viewpoint_elevation_consensus = 'Raised'
+            if photo.viewpoint_elevation == 2:
+                viewpoint_elevation_consensus = 'Aerial'
+    
+            return JsonResponse({'scene': scene, 'scene_consensus': scene_consensus, 'viewpoint_elevation': viewpoint_elevation, 'viewpoint_elevation_consensus': viewpoint_elevation_consensus})
         except:
             return JsonResponse({'error': _('Something went wrong')}, status=500)
     
