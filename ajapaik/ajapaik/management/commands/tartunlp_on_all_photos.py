b7a183c221e0b43079aa8cbf9a9481d698842c5c
Lauri Elias <lauri@ajapaik.ee>
2020-12-09 10:53:21 +0200

    Improve command a bit

--- ajapaik/ajapaik/management/commands/tartunlp_on_all_photos.py
+++ ajapaik/ajapaik/management/commands/tartunlp_on_all_photos.py
@@ -5,19 +5,33 @@ from ajapaik.ajapaik.models import Photo
 
 
 class Command(BaseCommand):
-    help = 'Connects to TartuNLP socket server and retrieves neuro machine translations for empty description fields'
+    help = 'Connects to TartuNLP API and retrieves neuro machine translations for empty description fields'
 
+    def add_arguments(self, parser):
+        parser.add_argument('number_of_photos', nargs='+', type=int, help='Batch size')
+
+    # TODO: Use event loop?
+    # event_loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(event_loop)
+    # tasks = [
+    #     asyncio.ensure_future(
+    #         self.get_single(each)
+    #     ) for each in photos
+    # ]
+    # done, _ = event_loop.run_until_complete(asyncio.wait(tasks))
+    # event_loop.close()
     def handle(self, *args, **options):
-        photos = Photo.objects.filter(
-            Q(description_et__isnull=False)
-            | Q(description_lv__isnull=False)
-            | Q(description_lt__isnull=False)
-            | Q(description_fi__isnull=False)
-            | Q(description_ru__isnull=False)
-            | Q(description_de__isnull=False)
-            | Q(description_en__isnull=False)
-        )
-        for each in photos[:100]:
-            print(f'Processing Photo {each.pk}')
-            each: Photo
-            each.fill_untranslated_fields()
+        if options['number_of_photos']:
+            photos = Photo.objects.filter(
+                Q(description_et__isnull=False)
+                | Q(description_lv__isnull=False)
+                | Q(description_lt__isnull=False)
+                | Q(description_fi__isnull=False)
+                | Q(description_ru__isnull=False)
+                | Q(description_de__isnull=False)
+                | Q(description_en__isnull=False)
+            )
+            for each in photos[:options['number_of_photos']]:
+                print(f'Processing Photo {each.pk}')
+                each: Photo
+                each.fill_untranslated_fields()
