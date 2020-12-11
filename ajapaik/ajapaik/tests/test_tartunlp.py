5e86055a1477b5c457fa710ab1cfac2a0188a510
Lauri Elias <lauri@ajapaik.ee>
2020-12-11 15:12:13 +0200

    Improve photos' TartuNLP command, move tests, activate more pytest plugins

--- ajapaik/ajapaik/tests/test_tartunlp.py
+++ ajapaik/ajapaik/tests/test_tartunlp.py
@@ -5,12 +5,8 @@ from ajapaik.ajapaik.models import Photo, Album
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize(('existing_key', 'existing_text'), [
-    ('description_et', 'Estonia teatrimaja, vaade Estonia puiesteelt'),
-    # ('description_fi', 'Rauman kanaalin meren puoleiselta osalta'),
-])
 @responses.activate
-def test_photos_tartunlp_translation(existing_key, existing_text):
+def test_photos_tartunlp_translation():
     for each in [
         'Igaunijas teātrimaja, skatā Igaunija puiesteelt',
         'Estijos teatro namai, vaizdas Estija puiesteelt',
@@ -20,8 +16,7 @@ def test_photos_tartunlp_translation(existing_key, existing_text):
         'Viro teatrimaja, vaade Viro puiesteelt'
     ]:
         responses.add(responses.GET, 'https://api.neurotolge.ee/v1.1/translate', json={'tgt': each})
-    test_instance = Photo()
-    setattr(test_instance, existing_key, existing_text)
+    test_instance = Photo(description_et='Estonia teatrimaja, vaade Estonia puiesteelt')
     test_instance.fill_untranslated_fields()
 
     assert 'teatrimaja' in getattr(test_instance, 'description_et')
@@ -34,11 +29,8 @@ def test_photos_tartunlp_translation(existing_key, existing_text):
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize(('existing_key', 'existing_text'), [
-    ('name_et', 'Stereofotosid Pariisist (Prantsusmaa)'),
-])
 @responses.activate
-def test_albums_tartunlp_translation(existing_key, existing_text):
+def test_albums_tartunlp_translation():
     for each in [
         'Stereofotos no Parīzes (Francija)',
         'Stereofotos iš Paryžiaus (Prancūzija)',
@@ -48,8 +40,7 @@ def test_albums_tartunlp_translation(existing_key, existing_text):
         'Stereofotot Pariisista (Ranska)'
     ]:
         responses.add(responses.GET, 'https://api.neurotolge.ee/v1.1/translate', json={'tgt': each})
-    test_instance = Album(atype=Album.CURATED)
-    setattr(test_instance, existing_key, existing_text)
+    test_instance = Album(name_et='Stereofotosid Pariisist (Prantsusmaa)', atype=Album.CURATED)
     test_instance.fill_untranslated_fields()
 
     assert 'Pariisist' in getattr(test_instance, 'name_et')
