b3bf1281633d0fd2b67c1d983a5255e3150f1df8
Mart Kaasik <mart.kaasik@gmail.com>
2022-01-02 17:58:08 +0200

    #79 Add Markdown toolbar for new comment, reply and edit fields

--- ajapaik/ajapaik/static/js/ajp-comments.js
+++ ajapaik/ajapaik/static/js/ajp-comments.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
     var photo_id = window.currentlyOpenPhotoId; //  Just to make name shorter.
 
+
+    // Simple Markdown editor toolbar
+    // Using: https://github.com/sparksuite/simplemde-markdown-editor
+    var addToolbar = function (div) {
+        var simplemde = new SimpleMDE({
+            element: $(div)[0],
+            spellChecker: false,
+            hideIcons: ["side-by-side", "fullscreen", "guide"],
+            forceSync: true,   // All the text is instantly copied to the original textarea div so the save works
+        });
+        $(div).attr('required', false);
+        $('.CodeMirror textarea').attr('required', true);
+        return simplemde;
+    }
+
+    addToolbar('#id_comment');
+    var reply_toolbar = null;
+    var edit_toolbar = null;
+
+
     // Disable hotkeys when typing message.
     $('#ajp-comments-container').on('focus', 'textarea', function () {
         window.hotkeysActive = false;
@@ -88,10 +108,15 @@ $(document).ready(function () {
         var comment_id = $(event.target).data('comment-id');
         var all_reply_form_divs = $('#ajp-comment-list div[class*="comment-reply-form-"]');
         var reply_form_div = $('#ajp-comment-list div[class~="comment-reply-form-' + comment_id + '"]');
+        var reply_textarea = reply_form_div.find('textarea');
 
         // Hide all forms and show only requested.
         all_reply_form_divs.addClass('d-none');
         reply_form_div.removeClass('d-none');
+
+        // Add Simple Markdown Editor toolbar
+        if (reply_toolbar == null)
+            reply_toolbar = addToolbar(reply_textarea);
 
         event.preventDefault();
     });
@@ -99,6 +124,12 @@ $(document).ready(function () {
 
     // Exit reply form (cancel button pressed).
     $('#ajp-comment-list').on('click', 'button[data-action="cancel"]', function(event) {
+        // Remove Simple Markdown Editor toolbar
+        if (reply_toolbar != null) {
+            reply_toolbar.toTextArea();
+            reply_toolbar = null;
+        }
+
         var all_reply_form_divs = $('#ajp-comment-list div[class*="comment-reply-form-"]');
         all_reply_form_divs.addClass('d-none');
     });
@@ -129,6 +160,10 @@ $(document).ready(function () {
         comment_container.addClass('d-none');
         edit_form_div.removeClass('d-none');
 
+        // Add Simple Markdown Editor toolbar
+        if (edit_toolbar == null)
+            edit_toolbar = addToolbar(comment_textarea);
+
         event.preventDefault();
     });
 
@@ -138,6 +173,11 @@ $(document).ready(function () {
         var comment_id = $(event.target).data('comment-id');
         var all_edit_form_divs = $('#ajp-comment-list div[class*="comment-edit-form-"]');
         var comment_container = $('#c' + comment_id + ' .comment-text');
+
+        if (edit_toolbar != null) {
+            edit_toolbar.toTextArea();
+            edit_toolbar = null;
+        }
 
         comment_container.removeClass('d-none');
         all_edit_form_divs.addClass('d-none');
