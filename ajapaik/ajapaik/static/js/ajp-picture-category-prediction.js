557f2ae6a5d9f7305c623c069e4f13cf93c25f57
angrun <angrun@ttu.ee>
2022-06-02 20:06:38 +0300

    [AJP-1] test

---
+++ ajapaik/ajapaik/static/js/ajp-picture-category-prediction.js
@@ -0,0 +1,23 @@
+function getPictureCategory(id) {
+    console.log("Here")
+    console.log(id)
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:7000/predict/test_2.jpg',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("X-CSRFTOKEN", window.docCookies.getItem('csrftoken'));
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (response) {
+            const category = response["category"];
+            const probability = parseFloat(response["probability"]).toFixed(2) * 100;
+            $("#ajp-probability1").html(category + " (" + probability + "%) :");
+        },
+        error: function (error) {
+            console.log(error)
+        }
+    });
+}
