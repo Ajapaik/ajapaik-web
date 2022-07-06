e90c4aba2cf81ca133e160362b7dca95132edb0f
angrun <angrun@ttu.ee>
2022-07-06 22:51:19 +0300

    test

--- ajapaik/ajapaik/static/js/ajp-picture-category-prediction.js
+++ ajapaik/ajapaik/static/js/ajp-picture-category-prediction.js
@@ -16,26 +16,26 @@ function getPictureCategory(id) {
             console.log(response);
             result = String(response).replaceAll("/", "-");
             console.log(result);
-            // $.ajax({
-            //     type: 'GET',
-            //     url: 'https://anna.ajapaik.ee/predict/' + result,
-            //     // url: 'http://localhost:7000/predict/' + result,
-            //     // beforeSend: function (xhr) {
-            //     //     xhr.setRequestHeader("X-CSRFTOKEN", window.docCookies.getItem('csrftoken'));
-            //     // },
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     },
-            //     success: function (response) {
-            //         const category = response["category"];
-            //         const probability = parseFloat(response["probability"]).toFixed(2) * 100;
-            //         $("#ajp-probability1").html(category + " (" + probability + "%) :");
-            //     },
-            //     error: function (error) {
-            //         console.log("Some error has occured: IN");
-            //         console.log(error)
-            //     }
-            // });
+            $.ajax({
+                type: 'GET',
+                url: 'https://anna.ajapaik.ee/predict/' + result,
+                // url: 'http://localhost:7000/predict/' + result,
+                // beforeSend: function (xhr) {
+                //     xhr.setRequestHeader("X-CSRFTOKEN", window.docCookies.getItem('csrftoken'));
+                // },
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                success: function (response) {
+                    const category = response["category"];
+                    const probability = parseFloat(response["probability"]).toFixed(2) * 100;
+                    $("#ajp-probability1").html(category + " (" + probability + "%) :");
+                },
+                error: function (error) {
+                    console.log("Some error has occured: IN");
+                    console.log(error)
+                }
+            });
         },
         error: function (error) {
             console.log("Some error has occured: OUT");
