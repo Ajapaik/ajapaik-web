76ae93c1eeb43541431f554628a03dfa9b8a2e73
angrun <angrun@ttu.ee>
2022-07-23 14:06:33 +0300

    test

--- ajapaik/ajapaik/static/js/ajp-picture-category-prediction.js
+++ ajapaik/ajapaik/static/js/ajp-picture-category-prediction.js
@@ -5,9 +5,9 @@ function getPictureCategory(id) {
         type: 'GET',
         // url: 'https://staging.ajapaik.ee/predict/image_2.jpg',
         url: 'https://anna.ajapaik.ee/photo-thumb-path/1',
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("X-CSRFTOKEN", window.docCookies.getItem('csrftoken'));
-        },
+        // beforeSend: function (xhr) {
+        //     xhr.setRequestHeader("X-CSRFTOKEN", window.docCookies.getItem('csrftoken'));
+        // },
         headers: {
             'Content-Type': 'text/plain'
         },
