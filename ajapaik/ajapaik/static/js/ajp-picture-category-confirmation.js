7db1605c07e26b8503745c017ffd5c99c62d74a3
angrun <angrun@ttu.ee>
2022-07-23 16:32:03 +0300

    test

--- ajapaik/ajapaik/static/js/ajp-picture-category-confirmation.js
+++ ajapaik/ajapaik/static/js/ajp-picture-category-confirmation.js
@@ -1,31 +1,19 @@
 function postPictureCategory(id, category) {
-    console.log(id);
     category = category.split(" ")[0];
+    console.log(id);
     console.log(category);
-    // console.log(id.id);
     console.log("I am in post");
     $.ajax({
         type: 'GET',
-        // url: 'https://staging.ajapaik.ee/predict/image_2.jpg',
         url: 'http://localhost:8000/photo-thumb-path/' + id,
-        // beforeSend: function (xhr) {
-        //     xhr.setRequestHeader("X-CSRFTOKEN", window.docCookies.getItem('csrftoken'));
-        // },
         headers: {
             'Content-Type': 'text/plain'
         },
         success: function (response) {
-            console.log("====");
-            console.log(response);
             imageUrl = String(response).replaceAll("/", "-");
-            console.log(result);
             $.ajax({
                 type: 'POST',
                 url: 'https://anna.ajapaik.ee/predict',
-                // url: 'http://localhost:7000/predict/' + result,
-                // beforeSend: function (xhr) {
-                //     xhr.setRequestHeader("X-CSRFTOKEN", window.docCookies.getItem('csrftoken'));
-                // },
                 data: {'category': category, 'image_url': imageUrl},
                 headers: {
                     'Content-Type': 'application/json',
@@ -35,13 +23,13 @@ function postPictureCategory(id, category) {
                     $("#ajp-category-confirmation").html("Done");
                 },
                 error: function (error) {
-                    console.log("Some error has occured: IN");
+                    console.log("FAILURE: IN");
                     console.log(error)
                 }
             });
         },
         error: function (error) {
-            console.log("Some error has occured: OUT");
+            console.log("FAILURE: OUT");
             console.log(error)
         }
     });
