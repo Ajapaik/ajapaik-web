93bbcaf141b287e9166483c8c7c7643c6d89e740
Kimmo Virtanen <kimmo.virtanen@gmail.com>
2022-11-06 18:48:41 +0000

    block indexing of opendata.ajapaik.ee using robots.txt. Add more paths to robots.txt

--- ajapaik/ajapaik/urls_opendata.py
+++ ajapaik/ajapaik/urls_opendata.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
-
 from ajapaik.ajapaik.opendata import PhotoViewSet, PhotoGeoTagViewSet
 
 router = DefaultRouter()
@@ -13,4 +13,5 @@ urlpatterns += format_suffix_patterns([
     url(r'^photos/(?P<pk>[0-9]+)/geotags/$',
         PhotoGeoTagViewSet.as_view({'get': 'retrieve'}),
         name='opendata-photo-geotags'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ])
