e3e72b035d924c2789b4a4116429be12d384b6fe
MaertHaekkinen <m4rth4kkinen@gmail.com>
2023-02-04 04:44:02 +0200

    fix(ci): add comment so that PyCharm won't skip importing test settings

--- ajapaik/settings/default.py
+++ ajapaik/settings/default.py
@@ -444,6 +444,6 @@ SOCIALACCOUNT_PROVIDERS = {
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36'
 
 if os.getenv('GITHUB_WORKFLOW'):
-    pass
+    from .test import *  # noqa - Needed so that PyCharm won't replace it with pass
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
