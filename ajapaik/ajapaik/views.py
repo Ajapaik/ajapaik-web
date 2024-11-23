8a6c788f9c517a81be85b93fac642ba17c471fb9
MaertHaekkinen <m4rth4kkinen@gmail.com>
2024-11-24 01:32:25 +0200

    Fix wrong total image count if no query parameter

--- ajapaik/ajapaik/views.py
+++ ajapaik/ajapaik/views.py
@@ -503,7 +503,7 @@ def _get_filtered_data_for_frontpage(request, album_id=None, page_override=None)
         # In some cases it is faster to get number of photos before we annotate new columns to it
         albumsize_before_sorting = 0
         if not album:
-            albumsize_before_sorting = Photo.objects.filter(pk__in=photos).cached_count(q)
+            albumsize_before_sorting = Photo.objects.filter(pk__in=photos).cached_count(str(filter_form.cleaned_data))
 
         # SORTING BELOW THIS LINE
 
