cb77a24127c93be4e730da05f4de003bcb5ea697
MaertHaekkinen <m4rth4kkinen@gmail.com>
2026-02-28 17:28:23 +0200

    test API

--- ajapaik/ajapaik/models.py
+++ ajapaik/ajapaik/models.py
@@ -1059,9 +1059,11 @@ class Photo(Model):
         if lat and lon:
             cached_response = GoogleMapsReverseGeocode.objects.filter(lat='{:.5f}'.format(lat),
                                                                       lon='{:.5f}'.format(lon)).first()
-            if cached_response and cached_response["response"]["status"] == "OK":
-                most_accurate_result = cached_response["response"]['results'][0]
-                self.address = most_accurate_result['formatted_address']
+            if cached_response and cached_response.get("response", {}).get("status") == "OK":
+                results = cached_response["response"].get("results", [])
+                if len(results) > 0:
+                    most_accurate_result = cached_response["response"]['results'][0]
+                    self.address = most_accurate_result['formatted_address']
 
     def set_backside(self, opposite):
         self.front_of = opposite
